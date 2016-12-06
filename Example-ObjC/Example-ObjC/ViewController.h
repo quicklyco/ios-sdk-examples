a745e9256b11cfce5fb443c0a2da6e1b2ef4720e
Anton Morozov <kardan@mini2012.local>
2016-12-06 14:26:30 +0200

    initial

---
+++ Example-ObjC/Example-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example-ObjC
+//
+//  Created by Anton Morozov on 12/6/16.
+//  Copyright © 2016 Anton Morozov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example-ObjC/Example-ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Example-ObjC
+//
+//  Created by Anton Morozov on 12/6/16.
+//  Copyright © 2016 Anton Morozov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
