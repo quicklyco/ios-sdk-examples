3d68d4dc0df94c3545178f8173ae5251c7773691
pruinis <pruinis@gmail.com>
2016-12-07 16:13:45 +0200

    remove xcode initial template comments

--- Example-ObjC/Example-ObjC/RootViewController.m
+++ Example-ObjC/Example-ObjC/RootViewController.m
@@ -21,12 +21,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
     [ZowdowSDK sharedInstance].appKey = @"some key";
     
     self.searchField.autocapitalizationType = UITextAutocapitalizationTypeNone;
@@ -38,11 +32,6 @@
     self.suggestionsLoader.delegate = self;
     
     self.tableView.rowHeight = 100;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark -
@@ -83,15 +72,5 @@
     }    
     return cell;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
